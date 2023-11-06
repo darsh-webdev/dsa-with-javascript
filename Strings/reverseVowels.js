868614f8546884568509cd4d06094ca637c9042b
darsh-webdev <rajputdarshanashok@gmail.com>
2023-11-07 01:01:48 +0530

    Completed String Questions

--- Strings/reverseVowels.js
+++ Strings/reverseVowels.js
@@ -1,12 +1,12 @@
 /*
 
-Solution for Reverse Words in a String: Given a string s, reverse 
+Solution for Reverse Vowels in a String: Given a string s, reverse 
 only all the vowels in the string and return it.
 
 The vowels are 'a', 'e', 'i', 'o', and 'u', and they can appear 
 in both lower and upper cases, more than once.
 
-e.g:Input: s = "leetcode"
+e.g: Input: s = "leetcode"
 Output: "leotcede"
 */
 
---
+++ Strings/rotateString.js
@@ -0,0 +1,36 @@
+/*
+
+Solution for Rotate String: Given two strings s and goal, return true 
+if and only if s can become goal after some number of shifts on s.
+
+A shift on s consists of moving the leftmost character of s to the rightmost 
+position.
+
+For example, if s = "abcde", then it will be "bcdea" after one shift.
+
+e.g: Input: s = "abcde", goal = "cdeab"
+Output: true
+*/
+
+function rotateString(s, goal) {
+  if (s.length !== goal.length) return false;
+
+  let len = s.length;
+
+  while (len > 0) {
+    s = s.slice(1).concat(s[0]);
+    if (s === goal) return true;
+    len--;
+  }
+
+  return false;
+
+  // Different Approach
+  //   return s.concat(s).includes(goal);
+}
+
+console.log(rotateString("abcde", "bcdea"));
+
+/*
+    Time Complexity: O(N)
+*/
